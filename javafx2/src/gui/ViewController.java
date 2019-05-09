6710ce0c1d8827affdd16af12d6c7acb30b2d93a
tchaves <tchaves@a5solutions.com>
2019-05-09 17:28:20 -0300

    Aulas JavaFX udemy

---
+++ javafx2/src/gui/ViewController.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+	@FXML
+	private Button btTest;
+	@FXML
+	public void onBtTestAction() {
+		System.out.println("Click!!");
+	}
+}
